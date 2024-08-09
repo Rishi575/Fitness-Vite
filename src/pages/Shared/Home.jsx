bcd0db367c3891a6f8422cef7c29bb22f4e070f9
Rishi575 <rishichidory@gmail.com>
2024-08-09 23:11:58 +0530

    Updated Home.jsx

--- src/pages/Shared/Home.jsx
+++ src/pages/Shared/Home.jsx
@@ -8,6 +8,7 @@ import Navbar from '@/components/Shared/Navbar'
 import Carousel from '@/components/ui/carousel'
 import Timeline from '@/components/ui/timeline'
 import Swip from '@/components/ui/swiper'
+import Categories from '@/components/ui/categories'
 
 const Home = () => {
     return (
@@ -24,30 +25,8 @@ const Home = () => {
             </div>
 
             <div className='flex justify-center items-center flex-col w-full'>
-                <div>
-                    <h1 className='text-3xl font-bold text-center mt-10'>Select by Categories</h1>
-                </div>
-                <div className='flex felx-row items-center h-52 w-full justify-evenly'>
-                    <div className=' h-32 w-1/4 bg-primary rounded-s rounded-e flex justify-center item-center hover:bg-slate-300 hover:transition-colors font-serif text-lg'>
-                        YOGA
-                    </div>
-                    <div className=' h-32 w-1/4 bg-primary rounded-s rounded-e flex justify-center item-center hover:bg-slate-300 hover:transition-colors font-serif text-lg'>
-                        FITNESS
-                    </div>
-                    <div className=' h-32 w-1/4 bg-primary rounded-s rounded-e flex justify-center item-center hover:bg-slate-300 hover:transition-colors font-serif text-lg'>
-                        BODY FITNESS
-                    </div>
-                </div>
-                <div className='flex felx-row items-center h-52 w-full justify-evenly -mt-8'>
-                    <div className=' h-32 w-1/4 bg-primary rounded-s rounded-e flex justify-center item-center hover:bg-slate-300 hover:transition-colors font-serif text-lg'>
-                        LAUGHING THERAPY
-                    </div>
-                    <div className=' h-32 w-1/4 bg-primary rounded-s rounded-e flex justify-center item-center hover:bg-slate-300 hover:transition-colors font-serif text-lg'>
-                        HEALTHY FOOD
-                    </div>
-                    <div className=' h-32 w-1/4 bg-primary rounded-s rounded-e flex justify-center item-center hover:bg-slate-300 hover:transition-colors font-serif text-lg'>
-                        NUTRITION
-                    </div>
+                <div className=''>
+                    <Categories/>
                 </div>
                 <div className=' mt-10 w-11/12'>
                     <Timeline />
