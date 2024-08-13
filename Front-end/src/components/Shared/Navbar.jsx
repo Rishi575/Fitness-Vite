99051cc4b3ec08e8226089e2b96bd855c4390ffd
Rishi575 <rishichidory@gmail.com>
2024-08-13 15:04:22 +0530

    Updated Navbar

--- Front-end/src/components/Shared/Navbar.jsx
+++ Front-end/src/components/Shared/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
       path: "/"
     },
     {
-      title: "About",
+      title: "Learn",
       path: "/about"
     },
     {
@@ -26,9 +26,6 @@ const Navbar = () => {
     <div className="relative top-0 left-0 w-full h-[8vh] flex flex-row justify-center items-center bg-background">
       <div className="w-28 h-full  font-bold flex justify-start items-center absolute left-0"><img src='https://ik.imagekit.io/wgp1xe3yu/Home/file%20(1).png?updatedAt=1723093770164' className='mt-5'/></div>
       <div className='w-2/4 h-full font-bold flex flex-row justify-center items-center gap-8'>
-        {/* <Link to='/'>Home </Link>
-        <Link to='/login'>Login </Link>
-        <Link to='/register'>Register </Link> */}
         {
           NavLinks.map((links, index) => (
             <li key={index} className='list-none'>
@@ -39,7 +36,7 @@ const Navbar = () => {
           ))
         }
       </div>
-      {/* <Button className="absolute right-5 rounded-3xl">Join Now</Button> */}
+      
       <div className="absolute right-5 flex-row flex gap-2">
       <ModeToggle/>
       <NavDropdown/>
