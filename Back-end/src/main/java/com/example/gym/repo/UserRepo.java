b0b0407ccc6b46d57761cc1f6cbe9b8289660fde
Rishi575 <rishichidory@gmail.com>
2024-08-17 17:18:20 +0530

    #User

--- Back-end/src/main/java/com/example/gym/repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.gym.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.gym.model.User;
-
-public interface UserRepo extends JpaRepository <User, Long>{
-
-}
