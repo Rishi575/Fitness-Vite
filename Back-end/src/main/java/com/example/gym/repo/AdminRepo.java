0071ef4a067a9ba6d26c4ef4113f1ede9fc5987e
Rishi575 <rishichidory@gmail.com>
2024-08-17 17:17:55 +0530

    #Admin

--- Back-end/src/main/java/com/example/gym/repo/AdminRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.gym.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.gym.model.Admin;
-
-public interface AdminRepo extends JpaRepository <Admin, Long> {
-}
